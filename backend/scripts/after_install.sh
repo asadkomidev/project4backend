bbe435f8053a3b377dc5c2a2253d7dd812f1837e
asadkomi <asadkomi@iastate.edu>
2023-05-08 19:12:50 -0500

    cicd

--- backend/scripts/after_install.sh
+++ backend/scripts/after_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo 'run after_install.sh: ' >> /home/ubuntu/project4backend/backend/deploy.log
 
-echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ubuntu/project4backend/backend/deploy.log
+echo 'cd /home/ubuntu/project4backend/backend' >> /home/ubuntu/project4backend/backend/deploy.log
 cd /home/ubuntu/project4backend/backend >> /home/ubuntu/project4backend/backend/deploy.log
 
 echo 'npm install' >> /home/ubuntu/project4backend/backend/deploy.log 
--- backend/scripts/application_start.sh
+++ backend/scripts/application_start.sh
@@ -2,5 +2,5 @@
 
 echo 'run application_start.sh: ' >> /home/ubuntu/project4backend/backend/deploy.log
 
-echo 'pm2 restart nodejs-express-app' >> /home/ubuntu/project4backend/backend/deploy.log
+echo 'pm2 restart project4backend' >> /home/ubuntu/project4backend/backend/deploy.log
 pm2 restart server.js >> /home/ubuntu/project4backend/backend/deploy.log
